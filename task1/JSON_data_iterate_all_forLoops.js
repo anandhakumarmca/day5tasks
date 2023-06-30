d730cf40adfc472ed04f1433b72f74710121eb73
Anandh Kumar <accetanandhmca@gmail.com>
2023-06-30 14:30:09 +0530

    Update JSON_data_iterate_all_forLoops.js

--- task1/JSON_data_iterate_all_forLoops.js
+++ task1/JSON_data_iterate_all_forLoops.js
@@ -1,3 +1,4 @@
+//JSON data
 const data = {
     "name": "Anandh",
     "age": 32,
@@ -5,23 +6,27 @@ const data = {
     "hobbies": ["netflix", "broswing", "gaming"],
     "address": "79/B, Valliyappa Garden, Palani , Tamilnadu, 624601"
   };
-  
+
+//iterate all JSON objects using simple for
   const keys = Object.keys(data);
   for (let i = 0; i < keys.length; i++) {
     const key = keys[i];
     const value = data[key];
     console.log(`${key}: ${value}`);
   }
-  
+
+//iterate all JSON objects using  for..in 
   for (const key in data) {
     console.log(`${key}: ${data[key]}`);
   }
-  
+
+  //iterate JSON Object hobbies data using  for..of 
   const hobbies = data.hobbies;
   for (const hobby of hobbies) {
     console.log(hobby);
   }
-  
+
+  //iterate all JSON objects using  for..each
   Object.keys(data).forEach((key) => {
     console.log(`${key}: ${data[key]}`);
   });
